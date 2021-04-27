c481b98070d0f7e5317b2536df79853b8aef7410
EugeneJoe <jmuthui70@gmail.com>
2021-04-27 13:19:46 +0000

    Add docstring

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
 def magic_calculation(a, b):
+    """Finds the result of a raised to the power b
+    Add 98 and return the resulting value"""
     b = a ** b
     return b + 98
