28ba1d14e313f4f1d0fe76c5d86237b427e91b3b
EugeneJoe <jmuthui70@gmail.com>
2021-04-29 14:06:18 +0000

    Remove sys module namespace

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 
 from calculator_1 import add, sub, mul, div
-import sys
+from sys import argv
 if __name__ == "__main__":
-    arg_num = len(sys.argv) - 1
+    arg_num = len(argv) - 1
     if arg_num != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    elif sys.argv[2] not in "+-*/":
+        exit(1)
+    elif argv[2] not in "+-*/":
         print("Unkown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
     else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] == "+":
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == "+":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif sys.argv[2] == "-":
+        elif argv[2] == "-":
             print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.argv[2] == "*":
+        elif argv[2] == "*":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.argv[2] == "/":
+        elif argv[2] == "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
