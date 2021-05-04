d6f82bbb8002fa466f49b68b97aeac7943b239cd
EugeneJoe <jmuthui70@gmail.com>
2021-05-04 17:13:55 +0000

    Use string in if condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@
 def multiple_returns(sentence):
     """Returns a tuple with the length of a string and its first character"""
     length = len(sentence)
-    if sentence[0]:
+    if sentence:
         first = sentence[0]
     else:
         first = None
