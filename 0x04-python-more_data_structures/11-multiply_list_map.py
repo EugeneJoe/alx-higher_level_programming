ba849a345f00454d017da19b96fed68baeba55f6
EugeneJoe <jmuthui70@gmail.com>
2021-05-06 12:07:46 +0000

    Use map function on a list to multiply each value by a number

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def multiply_list_map(my_list=[], number=0):
+    """Returns a list with all values multiplied by a number.
+       Does not use any loop.
+    """
+    new = list(map(lambda x: x * number, my_list))
+    return new
