378f8d38441da7437101cbd4cc7eeaf8ae8aca38
EugeneJoe <jmuthui70@gmail.com>
2021-05-06 15:16:35 +0000

    Return weighted average of all scores in list

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    """Returns the weighted average of all integers.
+
+       my_list format: each element is a tuple of the form
+                       (<score>, <weight>)
+    """
+    prod = 0
+    weight_sum = 0
+    for i in range(len(my_list)):
+        prod += my_list[i][0] * my_list[i][1]
+        weight_sum += my_list[i][1]
+    return prod/weight_sum
