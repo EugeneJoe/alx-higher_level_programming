2fc3c2cacd0f1d144378ba0c7fb2744b5767099c
EugeneJoe <jmuthui70@gmail.com>
2021-05-11 13:53:10 +0000

    main files and test files

---
+++ alias_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+alias jj="git add"
+alias kk="git commit -m"
+alias kl="git push origin master"
+alias em="emacs"
+alias ch="chmod a+x"
+alias gg="gcc -Wall -pedantic -Werror -Wextra"
+alias bt="betty"
