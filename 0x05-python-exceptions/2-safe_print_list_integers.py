114d35a52a83abbe482b6532dc8838084d6a11f5
EugeneJoe <jmuthui70@gmail.com>
2021-05-17 12:48:45 +0000

    Edit to conform to pep8 standards

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -16,7 +16,7 @@ def safe_print_list_integers(my_list=[], x=0):
         for n in range(0, x):
             if type(my_list[n]) is int:
                 print("{:d}".format(my_list[n]), end="")
-                count+=1
+                count += 1
         print()
     except IndexError as IE:
         raise
