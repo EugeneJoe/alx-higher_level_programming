e033b2b7406dccbcbd2a4a88262811c9dd96a192
EugeneJoe <jmuthui70@gmail.com>
2021-05-17 18:18:18 +0000

    Remove float cast

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,7 +15,7 @@ def list_division(my_list_1, my_list_2, list_length):
     res = 0
     for n in range(0, list_length):
         try:
-            res = float(my_list_1[n]) / my_list_2[n]
+            res = my_list_1[n] / my_list_2[n]
             res_1.append(res)
         except ZeroDivisionError:
             print("division by 0")
