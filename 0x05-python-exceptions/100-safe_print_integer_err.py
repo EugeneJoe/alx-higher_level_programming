18e3a799bc36bebb95af7cd81d3afb84c110b862
EugeneJoe <jmuthui70@gmail.com>
2021-05-17 18:22:02 +0000

    Restructure try-except block

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,10 +4,9 @@ import sys
 
 def safe_print_integer_err(value):
     """Prints an integer and handles exceptions"""
-
     try:
         print("{:d}".format(value))
+        return True
     except ValueError as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return False
-    return True
