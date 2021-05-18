654d496b035e703ac4bf23f014c53332fda244dd
EugeneJoe <jmuthui70@gmail.com>
2021-05-18 12:45:32 +0000

    Define a class Square with a private instance attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Contains a definition for a class Square"""
+
+
+class Square:
+    """Definition of a class Square."""
+
+    def __init__(self, size):
+        """Initializes an object of the class Square.
+
+        Args:
+            size (int): length of a side of the square
+
+        """
+        self.__size = size
