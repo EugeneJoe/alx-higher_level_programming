d5f4edf18fa6d81571321b9856ce3d6514a09db6
EugeneJoe <jmuthui70@gmail.com>
2021-05-23 11:20:48 +0000

    re-order if checks

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,12 +10,12 @@ def print_square(size):
             Must be a positive integer.
     """
 
+    if isinstance(size, float) and size < 0.0:
+        raise TypeError("size must be an integer")
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if isinstance(size, float) and size < 0.0:
-        raise TypeError("size must be an integer")
     if size == 0:
         print()
     else:
