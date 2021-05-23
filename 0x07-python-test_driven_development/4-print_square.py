6bd51b31a42cfe03e0ccd5aa357f9097a0202b1b
EugeneJoe <jmuthui70@gmail.com>
2021-05-23 11:35:47 +0000

    Restructure for statement

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -16,8 +16,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size == 0:
-        print()
-    else:
-        for i in range(0, size):
-            print('#' * size)
+
+    for i in range(size):
+        print('#' * size)
