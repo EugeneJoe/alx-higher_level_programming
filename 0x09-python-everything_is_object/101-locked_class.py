e5e1537adeb2494515f3d3924f97ca3e04919c9f
EugeneJoe <jmuthui70@gmail.com>
2021-05-25 18:20:25 +0000

    Use slots instead of __setattr__ method

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,16 +7,4 @@ Contains definition for class LockedClass.
 class LockedClass:
     """Definition of class LockedClass that only defines one attribute"""
 
-    def __setattr__(self, name, value):
-        """Called when dynamically creating a new instance attribute
-
-        Args:
-            name (str): name of new attribute being created
-            value (any): value of the attribute being created
-        """
-
-        if name == "first_name":
-            self.__dict__[name] = value
-        else:
-            raise AttributeError("'{}' object has no attribute '{}'".
-                                 format(self.__class__.__name__, name))
+    __slots__ = ['first_name']
