3c0183115965525bb91c779e5a440118f9f19db9
EugeneJoe <jmuthui70@gmail.com>
2021-05-26 18:22:50 +0000

    Use numpy library

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -14,44 +14,4 @@ def lazy_matrix_mul(m_a, m_b):
 
     """
 
-    # Check type of m_a and m_b
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-    # Check type of lists in m_a
-    for m1 in m_a:
-        if not isinstance(m1, list):
-            raise TypeError("m_a must be a list of lists")
-    if len(m_a) == 0:
-        raise ValueError("m_a can't be empty")
-    for m1 in m_a:
-        if len(m1) == 0:
-            raise ValueError("m_a can't be empty")
-        for m in m1:
-            if not isinstance(m, (int, float)):
-                raise TypeError("m_a should contain only integers or floats")
-    # Check type of lists in m_b
-    for m1 in m_b:
-        if not isinstance(m1, list):
-            raise TypeError("m_b must be a list of lists")
-    if len(m_b) == 0:
-        raise ValueError("m_b can't be empty")
-    for m1 in m_b:
-        if len(m1) == 0:
-            raise ValueError("m_b can't be empty")
-        for m in m1:
-            if not isinstance(m, (int, float)):
-                raise TypeError("m_b should contain only integers or floats")
-    if len(set([len(x) for x in m_a])) != 1:
-        raise TypeError("each row of m_a must be of the same size")
-    if len(set([len(x) for x in m_b])) != 1:
-        raise TypeError("each row of m_b must be of the same size")
-    columns_a = [len(x) for x in m_a]
-    rows_b = len(m_b)
-    for a in columns_a:
-        if a != rows_b:
-            raise ValueError("m_a and m_b can't be multiplied")
-
-    res = np.dot(m_a, m_b)
-    return res
+    return np.array(m_a).dot(np.array(m_b))
