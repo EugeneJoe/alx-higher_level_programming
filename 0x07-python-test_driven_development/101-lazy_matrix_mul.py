14847f45b373b57c17e2fa079f44dc8e4af18809
EugeneJoe <jmuthui70@gmail.com>
2021-05-26 20:06:23 +0000

    Check whether m_a and m_b are lists

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -14,4 +14,10 @@ def lazy_matrix_mul(m_a, m_b):
 
     """
 
+    # Check type of m_a and m_b
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
     return np.array(m_a).dot(np.array(m_b))
