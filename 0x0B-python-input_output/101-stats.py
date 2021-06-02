fce34aee40723d981fe8741bcc56070bfd20f797
EugeneJoe <jmuthui70@gmail.com>
2021-06-02 18:11:47 +0000

    Update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
-Script that reads stdin line by line and computes metrics
+Script that reads stdin line by line and computes relevant metrics
 """
 import sys
-import signal
 
 size = 0
 status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
