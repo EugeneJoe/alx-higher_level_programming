851a6496402ca6d860adedccc20aa95d7d2e2824
EugeneJoe <jmuthui70@gmail.com>
2021-06-08 13:30:16 +0000

    Fix initialization method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,7 +23,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if isinstance(value, int):
+        if type(value) is int:
             if value <= 0:
                 raise ValueError("width must be > 0")
             self.__width = value
@@ -37,9 +37,9 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if isinstance(value, int):
+        if type(value) is int:
             if value <= 0:
-                raise ValueError("height must be >= 0")
+                raise ValueError("height must be > 0")
             self.__height = value
         else:
             raise TypeError("height must be an integer")
