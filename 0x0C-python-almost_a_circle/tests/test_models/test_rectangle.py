8c33ec6470be56b635bb618ddaba3ad1ce597afd
EugeneJoe <jmuthui70@gmail.com>
2021-06-09 10:20:17 +0000

    Add more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,6 +2,8 @@
 Contains the definition of the test class for the Rectangle class.
 """
 import unittest
+import json
+import os
 from models.rectangle import Rectangle
 
 
@@ -12,23 +14,47 @@ class TestRectangleMethods(unittest.TestCase):
         """Set up resources required to run the tests"""
         self.rectangle_1 = Rectangle(2, 3)
         self.rectangle_2 = Rectangle(1, 3, 2, 1, 3)
+        self.rectangle_3 = Rectangle(2, 3, 4)
+        self.rectangle_4 = Rectangle(2, 3, 4, 5)
 
     def tearDown(self):
         """Tear down resources that had been set up to run tests"""
         del self.rectangle_1
         del self.rectangle_2
+        del self.rectangle_3
+        del self.rectangle_4
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
 
-    def test_width_height_assignment(self):
-        """Test assignment of width and height attributes"""
+    def test_width_assignment(self):
+        """Test assignment of width attributes"""
         self.assertEqual(self.rectangle_1.width, 2)
-        self.assertEqual(self.rectangle_1.height, 3)
+        self.assertEqual(self.rectangle_2.width, 1)
+        self.assertEqual(self.rectangle_3.width, 2)
+        self.assertEqual(self.rectangle_4.width, 2)
 
-    def test_x_y_assignment(self):
-        """Test assignment of x and y attributes"""
+    def test_height_assignment(self):
+        """Test assignment of height attributes"""
+        self.assertEqual(self.rectangle_1.height, 3)
+        self.assertEqual(self.rectangle_2.height, 3)
+        self.assertEqual(self.rectangle_3.height, 3)
+        self.assertEqual(self.rectangle_4.height, 3)
+
+    def test_x_assignment(self):
+        """Test assignment of x attributes"""
         self.assertEqual(self.rectangle_1.x, 0)
-        self.assertEqual(self.rectangle_1.y, 0)
         self.assertEqual(self.rectangle_2.x, 2)
+        self.assertEqual(self.rectangle_3.x, 4)
+        self.assertEqual(self.rectangle_4.x, 4)
+
+    def test_y_assignment(self):
+        """Test assignment of y attributes"""
+        self.assertEqual(self.rectangle_1.y, 0)
         self.assertEqual(self.rectangle_2.y, 1)
+        self.assertEqual(self.rectangle_3.y, 0)
+        self.assertEqual(self.rectangle_4.y, 5)
 
     def test_exceptions_type_str(self):
         """Test whether TypeError is raised for str arguments"""
@@ -143,13 +169,34 @@ class TestRectangleMethods(unittest.TestCase):
 
     def test_update(self):
         """Test the update method of the Rectangle class"""
-        self.rectangle_1.update(1, 1, 1, 1, 1)
-        self.assertEqual(str(self.rectangle_1), "[Rectangle] (1) 1/1 - 1/1")
+        rr = Rectangle(2, 3, 4, 5, 6)
+        rr.update(1)
+        self.assertEqual(str(rr), "[Rectangle] (1) 4/5 - 2/3")
+        rr.update(1, 2)
+        self.assertEqual(str(rr), "[Rectangle] (1) 4/5 - 2/3")
+        rr.update(1, 2, 3)
+        self.assertEqual(str(rr), "[Rectangle] (1) 4/5 - 2/3")
+        rr.update(1, 2, 3, 4)
+        self.assertEqual(str(rr), "[Rectangle] (1) 4/5 - 2/3")
+        rr.update(1, 2, 3, 4, 5)
+        self.assertEqual(str(rr), "[Rectangle] (1) 4/5 - 2/3")
 
     def test_update_kwargs(self):
         """Test the update method of the Rectangle class using *kwargs"""
         self.rectangle_1.update(height=1, width=2, id=5)
         self.assertEqual(str(self.rectangle_1), "[Rectangle] (5) 0/0 - 2/1")
+        kwargs = {"id": 1}
+        self.rectangle_1.update(**kwargs)
+        self.assertEqual(str(self.rectangle_1), "[Rectangle] (1) 0/0 - 2/1")
+        kwargs = {"id": 1, "width": 5}
+        self.rectangle_1.update(**kwargs)
+        self.assertEqual(str(self.rectangle_1), "[Rectangle] (1) 0/0 - 5/1")
+        kwargs = {"id": 1, "width": 5, "height": 4}
+        self.rectangle_1.update(**kwargs)
+        self.assertEqual(str(self.rectangle_1), "[Rectangle] (1) 0/0 - 5/4")
+        kwargs = {"id": 1, "width": 5, "height": 4, "x": 3}
+        self.rectangle_1.update(**kwargs)
+        self.assertEqual(str(self.rectangle_1), "[Rectangle] (1) 3/0 - 5/4")
         kwargs = {"width": 5, "height": 4, "x": 3, "y": 2, "id": 1}
         self.rectangle_1.update(**kwargs)
         self.assertEqual(str(self.rectangle_1), "[Rectangle] (1) 3/2 - 5/4")
@@ -160,3 +207,54 @@ class TestRectangleMethods(unittest.TestCase):
         kwargs = {"width": 5, "height": 4, "x": 3, "y": 2, "id": 1}
         self.rectangle_1.update(*args, **kwargs)
         self.assertEqual(str(self.rectangle_1), "[Rectangle] (1) 1/1 - 1/1")
+
+    def test_to_dictionary_method(self):
+        """Test the to_dictionary method of the Rectangle class"""
+        rr = Rectangle(2, 3, 0, 0, 89)
+        self.assertEqual(rr.to_dictionary(),
+                         {'width': 2, 'height': 3, 'x': 0, 'y': 0, 'id': 89})
+
+    def test_create_method(self):
+        """Test the create method of the Rectangle class"""
+        r1 = Rectangle(2, 7, 0, 0, 89)
+        r2 = Rectangle.create(**{'id': 89})
+        self.assertEqual(str(r1), str(r2))
+        r1 = Rectangle(1, 7, 0, 0, 89)
+        r2 = Rectangle.create(**{'id': 89, 'width': 1})
+        self.assertEqual(str(r1), str(r2))
+        r1 = Rectangle(1, 2, 0, 0, 89)
+        r2 = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual(str(r1), str(r2))
+        r1 = Rectangle(1, 2, 3, 0, 89)
+        r2 = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        self.assertEqual(str(r1), str(r2))
+        r1 = Rectangle(1, 2, 3, 4, 89)
+        r2 = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2,
+                                 'x': 3, 'y': 4})
+        self.assertEqual(str(r1), str(r2))
+
+    def test_save_to_file_method(self):
+        """Test the save_to_file method of the Rectangle class"""
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", mode="r", encoding="utf-8") as f:
+            data = json.load(f)
+        self.assertEqual(data, [])
+
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", mode="r", encoding="utf-8") as f:
+            data = json.load(f)
+        self.assertEqual(data, [])
+
+        Rectangle.save_to_file([Rectangle(1, 2, 3, 4, 5)])
+        with open("Rectangle.json", mode="r", encoding="utf-8") as f:
+            data = json.load(f)
+        self.assertEqual(data, [{"width": 1, "height": 2, "x": 3, "y": 4,
+                                 "id": 5}])
+
+    def test_load_from_file_method(self):
+        data = Rectangle.load_from_file()
+        self.assertEqual(data, [])
+
+        Rectangle.save_to_file([Rectangle(1, 2, 3, 4, 5)])
+        rrr = Rectangle.load_from_file()
+        self.assertEqual(str(rrr[0]), "[Rectangle] (5) 3/4 - 1/2")
