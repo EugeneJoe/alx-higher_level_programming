8f12a0ff6379bc1d9e1417cb28e69d1af4028560
EugeneJoe <jmuthui70@gmail.com>
2021-07-13 19:21:24 +0000

    Order results by genre name in ascending order

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- List all genres of the show 'Dexter' in hbtn_0d_tvshows database
-SELECT tv_genres.name
+SELECT tv_genres.name AS name
   FROM tv_genres
 
 INNER JOIN tv_show_genres
@@ -7,4 +7,6 @@ ON tv_genres.id = tv_show_genres.genre_id
 
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = "Dexter";
+WHERE tv_shows.title = "Dexter"
+
+ORDER BY name ASC;
