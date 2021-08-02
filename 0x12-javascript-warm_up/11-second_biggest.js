1e49df56efaab49ec444c2be4dfb05ea8502deb7
EugeneJoe <jmuthui70@gmail.com>
2021-08-02 20:18:42 +0000

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 
-if (process.argv.length < 3) {
+if (process.argv.length < 4) {
   console.log('0');
+  return;
 }
 const size = process.argv.length;
 const myInts = [];
-for (let i = 3; i < size; i++) {
-  myInts[i - 3] = parseInt(process.argv[i]);
+for (let i = 2; i < size; i++) {
+  myInts[i - 2] = parseInt(process.argv[i]);
 }
 myInts.sort(function (a, b) { return b - a; });
 console.log(myInts[1]);
