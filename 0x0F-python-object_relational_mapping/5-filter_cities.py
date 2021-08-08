ffea25d17b3edcf38bb28d7d4cd4d8c370760816
EugeneJoe <jmuthui70@gmail.com>
2021-08-08 12:05:57 +0000

    ORM python and sql files

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities of a state given as an argument
+from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                         password=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    SQL = "SELECT cities.name\
+           FROM cities\
+           INNER JOIN states\
+           ON cities.state_id = states.id\
+           WHERE states.name = %(username)s\
+           ORDER BY cities.id ASC"
+    cursor.execute(SQL, {'username': sys.argv[4]})
+    cities = cursor.fetchall()
+    if cities is not None:
+        for index, value in enumerate(cities):
+            if index != len(cities) - 1:
+                print("{}, ".format(value[0]), end="")
+            else:
+                print("{}".format(value[0]))
+        cursor.close()
+        db.close()
