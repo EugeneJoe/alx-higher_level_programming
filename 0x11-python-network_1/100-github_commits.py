3c5d9ef014ffa4a97d897b43391a5b66cbb80e0a
EugeneJoe <jmuthui70@gmail.com>
2021-08-20 19:53:59 +0000

    Python Github commits retrieval update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    u = 'https://api.github.com/repos/' + argv[2] + '/' + argv[1] + '/commits'
+    u = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
     resp = requests.get(u)
     json = resp.json()
     for i in range(0, 10):
