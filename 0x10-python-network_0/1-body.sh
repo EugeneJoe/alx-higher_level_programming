753dafd3cd777b6ac674fa9f198eb83a380791f9
EugeneJoe <jmuthui70@gmail.com>
2021-08-20 06:23:11 +0000

    Updates

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-# Takes a URL, sends a GET request to it and display the body of the response
-# only if the status code is 200
-
-# store the whole response with the status at the and
-HTTP_RESPONSE=$(curl --write-out "HTTPSTATUS:%{http_code}" -s -X GET -L "$1")
-
-# extract the body
-HTTP_BODY=${HTTP_RESPONSE//HTTPSTATUS:*/''}
-
-# extract the status
-HTTP_STATUS=$(echo "$HTTP_RESPONSE" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
-
-# example using the status
-if [  "$HTTP_STATUS" -eq 200  ]; then
-     echo -n "$HTTP_BODY"
-     exit 1
-fi
+# Takes a URL, sends a GET request and display the body if status code is 200
+curl -sL "$1"
