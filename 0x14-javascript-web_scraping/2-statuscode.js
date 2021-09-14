91488137b75357a767b3de6303b18e6a7782f52b
EugeneJoe <jmuthui70@gmail.com>
2021-09-14 20:23:30 +0000

    Use get method

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('code: ', response.statusCode);
-  }
+request
+    .get(process.argv[2])
+    .on(function(response)) {
+	console.log('code: ', response.statusCode);
+    }
 });
