3fc4177d9499e68f8ee50358a4aa63a065304bfe
EugeneJoe <jmuthui70@gmail.com>
2021-09-14 20:44:41 +0000

    Update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,8 +5,8 @@ request(process.argv[2], function (error, response, body) {
     console.log(error);
   } else {
     let movies = 0;
-    const data = JSON.parse(body).results;
-    for (const val of data) {
+    const data = JSON.parse(body);
+    for (const val of data.results) {
       if (val
         .characters
         .includes('https://swapi-api.hbtn.io/api/people/18/')) {
