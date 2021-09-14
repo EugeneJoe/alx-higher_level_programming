ac007901601ef1c2735ed42f932b9ce13446ebd1
EugeneJoe <jmuthui70@gmail.com>
2021-09-14 21:36:36 +0000

    Update: Hast wait for each request to complete before next is called

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    for (const val of data.characters) {
-      request.get(val, function (error, response, body1) {
-        if (error) {
-          console.log(error);
-        } else {
-          const name = JSON.parse(body1);
-          console.log(name.name);
-        }
-      });
-    }
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
