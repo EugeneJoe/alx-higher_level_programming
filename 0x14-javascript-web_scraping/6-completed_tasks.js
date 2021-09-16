f24353a4621c2ef55f9c5997b31e5f46f19bf177
EugeneJoe <jmuthui70@gmail.com>
2021-09-16 10:59:59 +0000

    Fix styling error

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,14 +2,13 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (error) {
-    console.log(error);
+    return console.log(error);
   } else {
     const data = JSON.parse(body);
-    let user = data[0].userId;
     const dict = {};
     for (const val of data) {
       if (val.completed) {
-	  dict[val.userId] = (dict[val.userId] || 0) + 1;
+        dict[val.userId] = (dict[val.userId] || 0) + 1;
       }
     }
     console.log(dict);
